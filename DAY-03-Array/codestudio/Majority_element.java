6db277594798a8d0a1c214fe7d53cdcf66fbdfa6
ritik sharma <sharmaritik896@gmail.com>
2022-06-12 01:11:45 +0530

    day4 problems solved

---
+++ DAY-03-Array/codestudio/Majority_element.java
@@ -0,0 +1,19 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int findMajority(int[] arr, int n) {
+		int count = 0;
+        int candidate = 0;
+        for(int num : arr){
+            if(count == 0) candidate = num;
+            if(candidate == num) count++;
+            else count--;
+        }
+        count = 0;
+        for(int num : arr){
+           if(num == candidate) count++;
+        }
+        if(count<=n/2) return -1;
+        return candidate;
+	}
+}
