061e1bf032deb2c36645e730bf8d547aacce6d1c
ritik sharma <sharmaritik896@gmail.com>
2022-06-15 23:53:49 +0530

    day5 solved

---
+++ Day-05-LinkedList/codestudio/DeleteNodeInALinkedList.java
@@ -0,0 +1,33 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    class LinkedListNode<T> {
+        T data;
+        LinkedListNode<T> next;
+        LinkedListNode(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+*****************************************************************/
+
+public class Solution {
+    public static void deleteNode(LinkedListNode<Integer> node) {
+     LinkedListNode<Integer>head = node;
+        LinkedListNode<Integer>prev = null;
+     
+           while(head.next != null){
+            int temp = head.next.data;
+        
+                head.data = temp;
+            prev = head;
+            head = head.next;
+        } 
+        prev.next = null;    
+    }
+}
